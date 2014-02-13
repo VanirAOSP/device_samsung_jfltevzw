845688320c9e563698ea1af04c518753db61e59c
nuclearmistake <nuclearmistake@gmail.com>
2014-02-12 23:10:37 -0500

    jfltevzw: access vzw apps in Play store
    Change-Id: Ib6d1861b0e32f0e39cacc46c9c186a02dccd6313

--- full_jfltevzw.mk
+++ full_jfltevzw.mk
@@ -27,6 +27,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from jfltevzw device
 $(call inherit-product, device/samsung/jfltevzw/device.mk)
 
+PRODUCT_GMS_CLIENTID_BASE := android-verizon
+
 # Set those variables here to overwrite the inherited values.
 PRODUCT_NAME := full_jfltevzw
 PRODUCT_DEVICE := jfltevzw
